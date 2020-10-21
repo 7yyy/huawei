90889e8fc08db624297496eaaccadd8e17b094fa
wlq <2495026956@q.com>
2020-10-21 10:52:33 +0800

    days3 marning

---
+++ src/api/server.js
@@ -0,0 +1,49 @@
+
+// 提供一个全站的api接口
+define(['jquery'],function($){
+
+    function getBannerData() {
+        return $.ajax('/api/mock/banner.json');
+    }
+    function getBanner2Data() {
+        return $.ajax('/api/mock/banner2.json');
+    }
+    function getPhoneData(){
+        return $.ajax('/api/mock/phone.json')
+    }
+    function getBookData(){
+        return $.ajax('/api/mock/book.json')
+    }
+    function getPadData(){
+        return $.ajax('/api/mock/pad.json')
+    }
+
+    return {
+        getBannerData,
+        getBanner2Data,
+        getPhoneData,
+        getBookData,
+        getPadData
+    };
+
+});
+
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/js/modules/banner.js
@@ -0,0 +1,52 @@
+
+
+
+define(['jquery'],function($){
+
+    var $banner = $('#banner');
+    function initBanner(res){
+        // console.log(res);
+        var tmp = `
+            <ul>
+                ${
+                    res.banner_list.map((v,i,a)=>{
+                        return`
+                        <li class="${i==0?'show':''}"><a href="${v.imgLink}"><img src="${v.imgUrl}"></a></li>
+                        `;
+                   }).join('')
+                }
+            </ul>
+            <ol>
+                ${
+                    res.banner_list.map((v,i,a)=>{
+                        return`
+                        <li class="${i==0?'active':''}"></li>
+                        `;
+                }).join('')
+                }
+        </ol> 
+        `;
+        $banner.html(tmp);
+        handleBanner();
+    }
+
+    function handleBanner(){
+        $banner.on('click','ol li',function(){
+            var $ulLis = $banner.find('ul li');
+            $(this).attr('class','active').siblings().attr('class','');
+            $ulLis.eq($(this).index()).attr('class','show').siblings().attr('class','');
+        });
+    }
+
+    return {initBanner};
+});
+
+
+
+
+
+
+
+
+
+
